bbc7d51432e798b4ef44f1f9f3821fbf475bf27e
Sallar Kaboli <sallar.kaboli@gmail.com>
2019-02-17 14:06:44 +0200

    Fix input prop update

--- test/FormattedInput.spec.js
+++ test/FormattedInput.spec.js
@@ -5,21 +5,13 @@ import { shallow } from "enzyme";
 import { FormattedInput, Presets } from "../source/index.js";
 
 test("accepts a value upon initialisation", function() {
-    const input = TestRenderer.create(
-        <FormattedInput
-            value="test value"
-            />
-    ).toTree();
+    const input = TestRenderer.create(<FormattedInput value="test value" />).toTree();
     expect(input.rendered.props.value).toEqual("test value");
 });
 
 test("supports optional <input> props", function() {
     const input = TestRenderer.create(
-        <FormattedInput
-            name="myInput"
-            placeholder="Your text here"
-            className="class1 class2"
-            />
+        <FormattedInput name="myInput" placeholder="Your text here" className="class1 class2" />
     ).toTree();
     expect(input.rendered.props.name).toEqual("myInput");
     expect(input.rendered.props.placeholder).toEqual("Your text here");
@@ -28,11 +20,7 @@ test("supports optional <input> props", function() {
 
 test("supports password type, which disables format", function() {
     const input = TestRenderer.create(
-        <FormattedInput
-            value="abc"
-            format={[{ char: /a/ }]}
-            type="password"
-            />
+        <FormattedInput value="abc" format={[{ char: /a/ }]} type="password" />
     ).toTree();
     expect(input.rendered.props.value).toEqual("abc");
     expect(input.rendered.props.type).toEqual("password");
@@ -46,52 +34,28 @@ test("it forces values to adhere to a pattern", function() {
         { char: /[0-9]/, repeat: 3 }
     ];
     const input = TestRenderer.create(
-        <FormattedInput
-            format={pattern}
-            value="bad19/2005extra"
-            />
+        <FormattedInput format={pattern} value="bad19/2005extra" />
     ).toTree();
     expect(input.rendered.props.value).toEqual("19/2005");
 });
 
 test("enforces maximum length through the use of a pattern", function() {
-    const pattern = [
-        { char: /./, repeat: 6 }
-    ];
+    const pattern = [{ char: /./, repeat: 6 }];
     const input = TestRenderer.create(
-        <FormattedInput
-            format={pattern}
-            value="123456789"
-            />
+        <FormattedInput format={pattern} value="123456789" />
     ).toTree();
     expect(input.rendered.props.value).toEqual("123456");
 });
 
 test("allows partial values", function() {
-    const pattern = [
-        { char: /[a-z]/i, repeat: 5 }
-    ];
-    const input = TestRenderer.create(
-        <FormattedInput
-            format={pattern}
-            value="a1bc"
-            />
-    ).toTree();
+    const pattern = [{ char: /[a-z]/i, repeat: 5 }];
+    const input = TestRenderer.create(<FormattedInput format={pattern} value="a1bc" />).toTree();
     expect(input.rendered.props.value).toEqual("abc");
 });
 
 test("supports repeating 'exactly' groups", function() {
-    const pattern = [
-        { char: /[a-z]/i },
-        { exactly: "*", repeat: 3 },
-        { char: /[a-z]/i }
-    ];
-    const input = TestRenderer.create(
-        <FormattedInput
-            format={pattern}
-            value="aaaa"
-            />
-    ).toTree();
+    const pattern = [{ char: /[a-z]/i }, { exactly: "*", repeat: 3 }, { char: /[a-z]/i }];
+    const input = TestRenderer.create(<FormattedInput format={pattern} value="aaaa" />).toTree();
     expect(input.rendered.props.value).toEqual("a***a");
 });
 
@@ -106,69 +70,38 @@ test("automatically enters delimiters", function() {
         { char: /[0-9]/, repeat: 4 }
     ];
     const input = TestRenderer.create(
-        <FormattedInput
-            format={pattern}
-            value="3204651290010002"
-            />
+        <FormattedInput format={pattern} value="3204651290010002" />
     ).toTree();
     expect(input.rendered.props.value).toEqual("3204-6512-9001-0002");
 });
 
 test("fires callback when value changes", function() {
-    const pattern = [
-        { char: /[0-9]/ },
-        { exactly: ":" },
-        { char: /[a-zA-Z]/ }
-    ];
+    const pattern = [{ char: /[0-9]/ }, { exactly: ":" }, { char: /[a-zA-Z]/ }];
     return new Promise(function(resolve) {
         const callback = function(formatted, raw) {
             expect(formatted).toEqual("3:a");
             expect(raw).toEqual("3a");
             resolve();
         };
-        const wrapper = shallow(
-            <FormattedInput
-                format={pattern}
-                onChange={callback}
-                />
-        );
+        const wrapper = shallow(<FormattedInput format={pattern} onChange={callback} />);
         wrapper.simulate("change", { target: { value: "3a" } });
     });
 });
 
 test("leaves the value empty if provided as such", function() {
-    const pattern = [
-        { char: /[0-9]/ },
-        { exactly: ":" },
-        { char: /[a-zA-Z]/ }
-    ];
-    const input = TestRenderer.create(
-        <FormattedInput
-            format={pattern}
-            value=""
-            />
-    ).toTree();
+    const pattern = [{ char: /[0-9]/ }, { exactly: ":" }, { char: /[a-zA-Z]/ }];
+    const input = TestRenderer.create(<FormattedInput format={pattern} value="" />).toTree();
     expect(input.rendered.props.value).toEqual("");
 });
 
 test("updates to empty correctly", function() {
-    const pattern = [
-        { char: /[0-9]/ },
-        { exactly: ":" },
-        { char: /[a-zA-Z]/ }
-    ];
+    const pattern = [{ char: /[0-9]/ }, { exactly: ":" }, { char: /[a-zA-Z]/ }];
     return new Promise(function(resolve) {
         const callback = function(value) {
             expect(value).toEqual("");
             resolve();
         };
-        const wrapper = shallow(
-            <FormattedInput
-                format={pattern}
-                value="3a"
-                onChange={callback}
-                />
-        );
+        const wrapper = shallow(<FormattedInput format={pattern} value="3a" onChange={callback} />);
         wrapper.simulate("change", { target: { value: "" } });
     });
 });
@@ -181,37 +114,30 @@ test("leaves out the last delimiter if the string is short", function() {
         { exactly: ":" },
         { char: /[a-z]/i }
     ];
-    const input = TestRenderer.create(
-        <FormattedInput
-            format={pattern}
-            value="5c"
-            />
-    ).toTree();
+    const input = TestRenderer.create(<FormattedInput format={pattern} value="5c" />).toTree();
     expect(input.rendered.props.value).toEqual("5:c");
 });
 
 test("supports presets", function() {
     const input = TestRenderer.create(
-        <FormattedInput
-            format={Presets.CreditCard}
-            value="3204651290010002"
-            />
+        <FormattedInput format={Presets.CreditCard} value="3204651290010002" />
     ).toTree();
     expect(input.rendered.props.value).toEqual("3204-6512-9001-0002");
 });
 
 test("supports custom components", function() {
-    const Comp = () => (
-        <span></span>
-    );
-    const testRenderer = TestRenderer.create(
-        <FormattedInput
-            element={Comp}
-            value="test"
-        />
-    );
+    const Comp = () => <span />;
+    const testRenderer = TestRenderer.create(<FormattedInput element={Comp} value="test" />);
     const testInstance = testRenderer.root;
     expect(testRenderer.toTree().rendered.props.value).toEqual("test");
     expect(testInstance.findByType("span")).toBeDefined();
     expect(() => testInstance.findByType("input")).toThrow();
+});
+
+test("updates props correctly", function() {
+    const input = TestRenderer.create(<FormattedInput value="test" />);
+    expect(input.toTree().rendered.props.value).toEqual("test");
+
+    input.update(<FormattedInput value="" />);
+    expect(input.toTree().rendered.props.value).toEqual("");
 });
