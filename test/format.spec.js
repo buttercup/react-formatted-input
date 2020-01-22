8e98ef18acc6445206c8a8f26f6f553caab10e5c
Perry Mitchell <perry@perrymitchell.net>
2020-01-22 21:46:04 +0200

    Add tests for new formatting

--- test/format.spec.js
+++ test/format.spec.js
@@ -1,21 +1,43 @@
 import { format } from "../source/index.js";
 
-test("outputs correct structure", function() {
-    const output = format("ab", [
-        { char: /\w/, repeat: 1 },
-        { exactly: "-" },
-        { char: /\w/, repeat: 1 }
-    ]);
-    expect(output).toHaveProperty("formatted", "a-b");
-    expect(output).toHaveProperty("raw", "ab");
-});
+describe("format", function() {
+    it("outputs correct structure", function() {
+        const output = format("ab", [
+            { char: /\w/, repeat: 1 },
+            { exactly: "-" },
+            { char: /\w/, repeat: 1 }
+        ]);
+        expect(output).toHaveProperty("formatted", "a-b");
+        expect(output).toHaveProperty("raw", "ab");
+    });
 
-test("recognises 'exactly' matches in original value", function() {
-    const output = format("a-b", [
-        { char: /\w/, repeat: 1 },
-        { exactly: "-" },
-        { char: /\w/, repeat: 1 }
-    ]);
-    expect(output).toHaveProperty("formatted", "a-b");
-    expect(output).toHaveProperty("raw", "ab");
+    it("recognises 'exactly' matches in original value", function() {
+        const output = format("a-b", [
+            { char: /\w/, repeat: 1 },
+            { exactly: "-" },
+            { char: /\w/, repeat: 1 }
+        ]);
+        expect(output).toHaveProperty("formatted", "a-b");
+        expect(output).toHaveProperty("raw", "ab");
+    });
+
+    it("outputs correct structure (string)", function() {
+        const output = format("ab", [
+            { char: "\\w", repeat: 1 },
+            { exactly: "-" },
+            { char: "\\w", repeat: 1 }
+        ]);
+        expect(output).toHaveProperty("formatted", "a-b");
+        expect(output).toHaveProperty("raw", "ab");
+    });
+
+    it("outputs correct structure (array)", function() {
+        const output = format("AB", [
+            { char: ["[ab]", "i"], repeat: 1 },
+            { exactly: "-" },
+            { char: ["[ab]", "i"], repeat: 1 }
+        ]);
+        expect(output).toHaveProperty("formatted", "A-B");
+        expect(output).toHaveProperty("raw", "AB");
+    });
 });
